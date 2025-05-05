9d2e885c38341cf37d3e742fdcd04e62b5fb9671
Mosley <parthoroy158@gmail.com>
2025-05-05 11:59:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://newpractice2025:hPX3BEraSHwIMmpX@cluster0.qbyxney.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = const uri = process.env.MONGODB_URI;;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
