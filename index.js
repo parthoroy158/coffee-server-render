12a238c22086d4964abb24fa0a20da53bd363ee6
Mosley <parthoroy158@gmail.com>
2025-05-05 12:04:53 +0600

    final commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const uri = const uri = process.env.MONGODB_URI;;
+const uri = process.env.MONGODB_URI;;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
